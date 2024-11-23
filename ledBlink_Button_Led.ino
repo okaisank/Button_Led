fa656718d91ccff768f9b86dcc4a943be0c1217e
Joevohan261 <84632709+okaisank@users.noreply.github.com>
2024-11-23 13:55:29 +0700

    Button_Led.ino

--- ledBlink_Button_Led.ino
+++ ledBlink_Button_Led.ino
@@ -1,5 +1,4 @@
-int led1 = 12; // กำหนดขาใช้งาน
-int led2 = 13; // กำหนดขาใช้งาน
+int led1 = 13; // กำหนดขาใช้งาน
 int buttonPin = 2;
 int buttonState = 0;
 void setup()
@@ -8,22 +7,15 @@ pinMode(led1, OUTPUT); // กำหนดขาทำหน้าที่ให
 pinMode(buttonPin, INPUT);// กำหนดขาทำหน้าที่ให้ขา 3 เป็น INPUT รับค่าจากสวิตช์
 digitalWrite(led1, LOW);
 }
-
-void myFunction(blinkRate){
-  digitalWrite(led2, HIGH);
-  delay(blinkRate);
-  digitalWrite(led2, LOW);
-  delay(blinkRate);
-
-  
-}
-
-
 void loop()
 {
 buttonState = digitalRead(buttonPin); // อ่านค่าสถานะขา3
 if (buttonState == HIGH) { //กำหนดเงื่อนไขถ้าตัวแปล buttonState เก็บ ค่า 1(HIGH) ให้ทำในปีกกา
 digitalWrite(led1, HIGH); // ไฟ LED 1ติด
+}
+else { //ถ้าตัวแปล buttonState ไม่ได้เก็บ ค่า 1(HIGH) คือ ตัวแปล buttonState เก็บค่า 0(LOW) อยู่ ให้ทำปีกกาข้างล่าง
+digitalWrite(led1, LOW); // ไฟ LED 1ดับ
+}
 }
 else { //ถ้าตัวแปล buttonState ไม่ได้เก็บ ค่า 1(HIGH) คือ ตัวแปล buttonState เก็บค่า 0(LOW) อยู่ ให้ทำปีกกาข้างล่าง
 digitalWrite(led1, LOW); // ไฟ LED 1ดับ
